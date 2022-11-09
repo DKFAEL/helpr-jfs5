f8d221a338c97baec40b711175fa423c89b70942
DKFAEL <rafaelnunesemp@hotmail.com>
2022-11-09 11:37:45 -0300

    Crud funcionario

--- src/main/java/org/soulcodeacademy/helpr/services/FuncionarioService.java
+++ src/main/java/org/soulcodeacademy/helpr/services/FuncionarioService.java
@@ -1,6 +1,8 @@
 package org.soulcodeacademy.helpr.services;
 
+import org.soulcodeacademy.helpr.domain.Cargo;
 import org.soulcodeacademy.helpr.domain.Funcionario;
+import org.soulcodeacademy.helpr.domain.dto.FuncionarioDTO;
 import org.soulcodeacademy.helpr.repositories.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,9 @@ public class FuncionarioService {
     @Autowired
     private FuncionarioRepository funcionarioRepository;
 
+    @Autowired
+    private  CargoService cargoService;
+
     public List<Funcionario> listar(){
         return this.funcionarioRepository.findAll();  //FindAll (todo mundo)
     }
@@ -22,7 +27,35 @@ public class FuncionarioService {
         if (funcionario.isEmpty()){
             throw new RuntimeException("O funcionário não foi encontrado!");
         } else {
-            return  funcionario.get(); // pegaovalor da entidade encontrada
+            return  funcionario.get(); // pega o valor da entidade encontrada
         }
+    }
+
+    public  Funcionario salvar (FuncionarioDTO dto){
+        Cargo cargo =this.cargoService.getCargo(dto.getIdCargo()); // verifica se o cargo existe mesmo
+        // id, nome, email, cpf, String senha, foto, cargo
+        Funcionario funcionario = new Funcionario(null, dto.getNome(),  dto.getEmail(),  dto.getCpf(), dto.getSenha() , dto.getFoto(), cargo);
+        Funcionario salvo = this.funcionarioRepository.save(funcionario);
+        return salvo;
+    }
+
+    public Funcionario atualizar(Integer idFuncionario, FuncionarioDTO dto){
+        //Busca o funcionario com o idFuncionario
+        Funcionario funcionarioAtual = this.getFuncionario(idFuncionario);
+        Cargo cargo = this.cargoService.getCargo(dto.getIdCargo());
+        funcionarioAtual.setNome(dto.getNome());
+        funcionarioAtual.setEmail(dto.getEmail());
+        funcionarioAtual.setCpf(dto.getCpf());
+        funcionarioAtual.setSenha(dto.getSenha());
+        funcionarioAtual.setFoto(dto.getFoto());
+        funcionarioAtual.setCargo(cargo);
+
+        Funcionario atualizado = this.funcionarioRepository.save(funcionarioAtual);
+        return atualizado;
+    }
+
+    public  void deletar(Integer idFuncionario){
+        Funcionario funcionario = this.getFuncionario(idFuncionario);
+        this.funcionarioRepository.delete(funcionario);
     }
 }
